ab69f832ce5a51ab3ec00e931a0f1c1b1e16f055
DESKTOP-GE8TEVS\user <asd>
2023-03-16 18:41:54 +0900

    [박주영] - SNS 프로젝트 최초파일

---
+++ src/main/java/com/doongie/instagram/InstagramApplication.java
@@ -0,0 +1,16 @@
+package com.doongie.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class InstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/doongie/instagram/test/HelloController.java
@@ -0,0 +1,23 @@
+package com.doongie.instagram.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		
+		return "Hello World";
+	}
+	
+	@GetMapping("/hello/jsp")
+	public String helloJsp() {
+		
+		return "hello";
+	}
+
+}
